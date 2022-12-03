f5a276104cf904402cd3aece66527156e9b8b24f
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-12-03 00:09:24 -0300

    Projeto criado

---
+++ src/main/java/com/jdkclean/jdkcommerce/JdkcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.jdkclean.jdkcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdkcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdkcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdkclean/jdkcommerce/JdkcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdkclean.jdkcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
