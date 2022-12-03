a191e6ddc74111d6b72b4f0a56c7d39b6f9a0cbe
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-12-03 13:57:45 -0300

    DTO

--- src/main/java/com/jdkclean/jdkcommerce/services/CategoryService.java
+++ src/main/java/com/jdkclean/jdkcommerce/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.jdkclean.jdkcommerce.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jdkclean.jdkcommerce.dto.CategoryDTO;
 import com.jdkclean.jdkcommerce.entities.Category;
 import com.jdkclean.jdkcommerce.repositories.CategoryRepository;
 
@@ -16,8 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 	
 }
