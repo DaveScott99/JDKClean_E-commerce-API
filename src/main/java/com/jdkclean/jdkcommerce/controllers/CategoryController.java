5ca50e5282a4a2f26dd68a76846a026ec8dbb65e
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-12-03 17:02:07 -0300

    Post Category

--- src/main/java/com/jdkclean/jdkcommerce/controllers/CategoryController.java
+++ src/main/java/com/jdkclean/jdkcommerce/controllers/CategoryController.java
@@ -1,13 +1,17 @@
 package com.jdkclean.jdkcommerce.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jdkclean.jdkcommerce.dto.CategoryDTO;
 import com.jdkclean.jdkcommerce.services.CategoryService;
@@ -29,6 +33,14 @@ public class CategoryController {
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
 		CategoryDTO category = service.findById(id);
 		return ResponseEntity.ok().body(category);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+		
 	}
 
 }
